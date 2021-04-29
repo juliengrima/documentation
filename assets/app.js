0c076a16d35035acbd990e6d6cf8c26d0d3d8407
Julien <gogrinformatique@gmail.com>
2021-04-29 21:42:54 +0200

    compile webpack

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,6 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/noscript.css';
 import './styles/main.css';
-import './styles/fontawesome-all.min.css';
 import './styles/app.css';
 
 // start the Stimulus application
